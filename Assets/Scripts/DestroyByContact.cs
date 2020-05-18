24bf20ac29075b9e370594dd324cb6d02fe046d7
Owen Dugmore <oadugmore@outlook.com>
2020-05-18 12:57:55 -0700

    remove unnecessary methods

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -15,16 +15,4 @@ public class DestroyByContact : MonoBehaviour
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
